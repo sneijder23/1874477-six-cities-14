237ceff17a13ec580dc53023ae6bab4b9f71c011
Alexander <sneijder23@ya.ru>
2023-12-15 02:21:01 +0300

    fixed deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/1874477-six-cities-14/',
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',

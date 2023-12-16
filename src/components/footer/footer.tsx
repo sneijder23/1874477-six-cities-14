cf4f8e759f1a5fd18b3945bc9079ac0cb79c8c1b
Alexander <sneijder23@ya.ru>
2023-12-16 12:28:21 +0300

    fixed src footer logo

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
       <Link className="footer__logo-link" to={AppRoute.Root}>
         <img
           className="footer__logo"
-          src="img/logo.svg"
+          src="1874477-six-cities-14/img/logo.svg"
           alt="6 cities logo"
           width="64"
           height="33"
